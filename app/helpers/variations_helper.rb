6c5bc8caad85b62e60eb72c341f098d6839304a5
mariapaz <mpazcarvacho@gmail.com>
2021-12-09 14:00:27 -0300

    only show variations with stock

--- app/helpers/variations_helper.rb
+++ app/helpers/variations_helper.rb
@@ -1,9 +1,10 @@
 module VariationsHelper
-  def colors_for_select(product_id)
-    Variation.where(product_id: product_id).map{|p| [p.color.description, p.color_id]}
-  end
+  
 
   def variations_for_select(product_id)
-    Variation.where(product_id: product_id).map{|p| ["#{p.size.description} - #{p.color.description}", p.size_id]}
+    # DONE only return variations with stock.
+    Variation.where(['stock > ? and product_id = ?', 0, product_id ]).map{|p| ["#{p.size.description} - #{p.color.description}", p.size_id]}
   end
 end
+
+# product_id: product_id, 
--- app/models/order.rb
+++ app/models/order.rb
@@ -24,6 +24,7 @@ class Order < ApplicationRecord
   end
 
   def add_product(product_id, quantity)
+    # TODO update stock in variations. Add variations field to  orderitem before.
     product = Product.find(product_id)
     if product && product.stock > 0
       order_items.create(product_id: product.id, quantity: quantity, price: product.price)
